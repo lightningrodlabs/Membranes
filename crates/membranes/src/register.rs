a53235a986e9d10d00095ff0ccc5253202241607
damien <doute.d@gmail.com>
2023-02-02 13:50:52 -0500

    Cleaner register

--- crates/membranes/src/register.rs
+++ crates/membranes/src/register.rs
@@ -6,43 +6,19 @@ use membranes_types::ThresholdType;
 
 #[hdk_extern]
 pub fn register_threshold_type(tt: ThresholdType) -> ExternResult<ActionHash> {
-   //let tt = ThresholdType { name: type_name };
-   debug!("register_threshold_type() {:?}", tt);
+   debug!("register_threshold_type() {}", tt.name);
    let ah = create_entry(MembranesEntry::ThresholdType(tt))?;
    Ok(ah)
 }
 
 
-
 ///
 #[hdk_extern]
 pub fn get_all_registered_threshold_types(_ : ()) -> ExternResult<Vec<ThresholdType>> {
-   let entry_type = EntryType::App(AppEntryDef {
-      entry_index: get_index::<MembranesEntry>(MembranesEntryTypes::ThresholdType)?.into(),
-      zome_index: 0.into(), //FIXME broken zome_info()?.id,
-      visibility: EntryVisibility::Public,
-   });
+   debug!("get_all_registered_threshold_types() zome_info = {:?}", zome_info()?);
+   let entry_type = MembranesEntryTypes::ThresholdType.try_into().unwrap();
    debug!("get_all_registered_threshold_types() entry_type = {:?}", entry_type);
-   //let res = get_all_typed_local(entry_type);
-
-   let query_args = ChainQueryFilter::default()
-      .include_entries(true)
-      .action_type(ActionType::Create)
-      .entry_type(entry_type);
-   let records = query(query_args)?;
-
-   debug!("get_all_registered_threshold_types() records = {:?}", records);
-
-   /// Get typed for all results
-   let mut typeds = Vec::new();
-   for record in records {
-      let typed: ThresholdType = get_typed_from_record(record.clone())?;
-      typeds.push(typed)
-   }
-
-
-   debug!("get_all_registered_threshold_types() res = {:?}", typeds);
-   return Ok(typeds);
+   return get_all_typed_local(entry_type);
 }
 
 
